ec37b4c6f6bf873b5384ef5cfb55232b26e77c5e
Justus-Elolo <jasimrootie@gmail.com>
2024-07-29 06:23:20 +0000

    Quick fix

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -1,23 +1,33 @@
-
 import React from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
 import Notifications from '../Notifications/Notifications';
 import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
+function App({ isLoggedIn }) {
 	return (
 		<React.Fragment>
-			<Notifications />
 			<div className='App'>
-				<Header />
-				<Login />
+				<div className='heading-section'>
+					<Notifications />
+					<Header />
+				</div>
+				{isLoggedIn ? <CourseList /> : <Login />}
 				<Footer />
 			</div>
 		</React.Fragment>
 	);
 }
 
-export default App;
+App.defaultProps = {
+	isLoggedIn: false,
+};
 
+App.propTypes = {
+	isLoggedIn: PropTypes.bool,
+};
+
+export default App;
