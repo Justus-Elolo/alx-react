2ed3b384e9b611a325d5bd75dba2fc64a0f9a118
Justus-Elolo <jasimrootie@gmail.com>
2024-08-01 09:05:21 +0000

    App updated

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -10,6 +10,12 @@ import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+	}
+
 	listCourses = [
 		{ id: 1, name: 'ES6', credit: 60 },
 		{ id: 2, name: 'Webpack', credit: 20 },
@@ -21,6 +27,20 @@ class App extends React.Component {
 		{ id: 2, type: 'urgent', value: 'New resume available' },
 		{ id: 3, type: 'urgent', html: getLatestNotification() },
 	];
+
+	handleKeyPress(e) {
+		if (e.ctrlKey && e.key === 'h') {
+			alert('Logging you out');
+			this.props.logOut();
+		}
+	}
+	componentDidMount() {
+		document.addEventListener('keydown', this.handleKeyPress);
+	}
+
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.handleKeyPress);
+	}
 
 	render() {
 		return (
@@ -44,10 +64,14 @@ class App extends React.Component {
 
 App.defaultProps = {
 	isLoggedIn: false,
+	logOut: () => {
+		return;
+	},
 };
 
 App.propTypes = {
 	isLoggedIn: PropTypes.bool,
+	logOut: PropTypes.func,
 };
 
 export default App;
--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -1,3 +1,7 @@
+
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
 import App from './App';
 import Login from '../Login/Login';
@@ -5,7 +9,7 @@ import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 describe('App tests', () => {
 	it('renders without crashing', () => {
@@ -47,3 +51,40 @@ describe('App tests', () => {
 		expect(component.contains(<Login />)).toBe(false);
 	});
 });
+
+describe('When ctrl + h is pressed', () => {
+	it('calls logOut function', () => {
+		const mocked = jest.fn();
+		const wrapper = mount(<App logOut={mocked} />);
+		const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' });
+		document.dispatchEvent(event);
+
+		expect(mocked).toHaveBeenCalledTimes(1);
+		wrapper.unmount();
+	});
+
+	window.alert = jest.fn();
+	it('checks that alert function is called', () => {
+		const wrapper = mount(<App />);
+		const spy = jest.spyOn(window, 'alert');
+		const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' });
+		document.dispatchEvent(event);
+
+		expect(spy).toHaveBeenCalled();
+		spy.mockRestore();
+		wrapper.unmount();
+	});
+
+	it('checks that the alert is "Logging you out"', () => {
+		const wrapper = mount(<App />);
+		const spy = jest.spyOn(window, 'alert');
+		const event = new KeyboardEvent('keydown', { ctrlKey: true, key: 'h' });
+		document.dispatchEvent(event);
+
+		expect(spy).toHaveBeenCalledWith('Logging you out');
+		jest.restoreAllMocks();
+		wrapper.unmount();
+	});
+	window.alert.mockClear();
+});
+
