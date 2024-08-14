1ba35a43c8f0ca85dbf403e1cf63a8d11a0b8583
Justus-Elolo <jasimrootie@gmail.com>
2024-08-14 11:52:17 +0000

    Modifications

--- 0x06-React_Immutable/0-from.js
+++
@@ -1,6 +0,0 @@
-// import { fromJS } from 'immutable';
-import { fromJS } from './node_modules/immutable/dist/immutable';
-
-const getImmutableObject = (object) => fromJS(object);
-
-export default getImmutableObject;
