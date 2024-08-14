07eea071e26a0e5127bf3136030f6911f6c25269
Justus-Elolo <jasimrootie@gmail.com>
2024-08-14 09:48:09 +0000

    Lists from arrays

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,7 @@
+
+import { List, Map } from 'immutable';
+
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
+
+export const mergeElements = (page1, page2) => Map(page1).concat(Map(page2));
+
