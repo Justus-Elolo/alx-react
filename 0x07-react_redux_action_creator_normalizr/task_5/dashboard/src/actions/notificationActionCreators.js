7d6dc4d946b55a7dbc3254281fe2d5b6068f2d65
Justus-Elolo <jasimrootie@gmail.com>
2024-08-20 13:37:30 +0000

    actions for the notification list

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,15 @@
+import { MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
+
+export function markAsRead(index) {
+    return {
+        type: MARK_AS_READ,
+        index: index,
+    }
+}
+
+export function setNotificationFilter(filter) {
+    return {
+        type: SET_TYPE_FILTER,
+        filter: filter
+    }
+}
